ffda665262de71e368352bdc98657a9194a946f3
geek2pm <59955605+geek2pm@users.noreply.github.com>
2020-02-29 20:15:25 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -9,3 +9,54 @@ CREATE TABLE device (
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8
 COLLATE=utf8_general_ci;
+
+CREATE TABLE user (
+	id INT auto_increment NOT NULL,
+	name varchar(32) NOT NULL,
+	passwd varchar(256) NOT NULL,
+	create_time TIMESTAMP NOT NULL,
+	first_login TIMESTAMP NULL,
+	last_login TIMESTAMP NULL,
+	primary key (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+CREATE TABLE user (
+	id INT auto_increment NOT NULL,
+	name varchar(32) NOT NULL,
+	passwd varchar(256) NOT NULL,
+	create_time TIMESTAMP NOT NULL,
+	first_login TIMESTAMP NULL,
+	last_login TIMESTAMP NULL,
+	primary key (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+CREATE TABLE value (
+	id INT auto_increment NOT NULL,
+	name varchar(32) NOT NULL,
+	value varchar(256) NOT NULL,
+	value_type varchar(8) NOT NULL,
+	create_time TIMESTAMP NOT NULL,
+	device_key varchar(32) NOT NULL,
+	primary key (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+CREATE TABLE cmd (
+	id INT auto_increment NOT NULL,
+	cmd varchar(256) NOT NULL,
+	create_time TIMESTAMP NOT NULL,
+	is_execute INT(1) NOT NULL,
+	device_key varchar(32) NOT NULL,
+	primary key (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;;
